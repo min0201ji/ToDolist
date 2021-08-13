4a038fd8bd4784395bd4799f3d822751d136aa34
minji <ymcm2468@gmail.com>
2021-08-13 12:50:03 +0900

    1.  가상황경 생성 - 프로젝트
    2. Django install(터미널)
    
        - pip install django
    
    3. Django 프로젝트 시작
    
       Django-admin startproject ToDoList
    
       cd ToDolist (cd .. 은 뒤로)
    
       ls 로 확인
    
    4. django app 시작(필요한 만큼 생성)
    
       python manage.py startapp my_to_do_app
    
       4.1
    
       생성된 app은 프로젝트에 등록 해야 함
    
       ** 생성된 파이참 프로젝트 이름: ToDoList
    
       ** 생성된 장고 프로젝트 이름 : ToDoList > settings.py 에 앱 등록
    
       ** 생성된 app 이름 : my_to_do_app
    
       - settings.py 파일 열고
    
         INSTALLED_APPS = [] 에 앱 이름 등록
    
    5.  Django migrate 진행
    
       데이터베이스에 테이블, 필드 등의 변경이 발생했을 때 지정된 데이터베이스에 적용하는 과정을 의미
    
       - 프로젝트 생성 후 기본 마이그레이션 진행
    
         => python manage.py migrate
    
       - python manage.py runserver 으로 환경설정 확인
    
    ##### 8월12일(목) ToDoList 두번째 #####
    
    #### 8월13일(금) Error: That port is already in use 라는 오류 발생
    => sudo lsof -t -i tcp:8000 | xargs kill -9 입력발생
    pycharm terminal로 했으나, mac은 보안이 철저하고 자세한것으로 오류가 많이 나서 mac에 있는 terminal을 쓰기로 함.(p.s. 그래서 개발자들이 mac을 많이 쓴다고 한다. 오류가 자주 나지만, 그만큼 공부할 수 있으니 맥을 선택한건 좋은 선택이라 생각한다!)
    => mac terminal에 sudo lsof -t -i tcp:8000 | xargs kill -9을 직접 입력 후 컴퓨터 비번 입력 다시 python manage.py runserver 입력하니 실햄 됨.

--- ToDoList/my_to_do_app/views.py
+++ ToDoList/my_to_do_app/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect # 특정 url을 재 요청해주는 함수
+from django.urls import reverse # 기존 요청임을 의미하는 함수
 from .models import *
 # Create your views here.
 
 def index(request) :
-    return render(request,'my_to_do_app/index.html') # Templates/my_to_do_app/index.html
+    # DB의 m̆̈y_to_do_app_todo 테이블에서 모든 row를 select
+    todos = Todo.objects.all() # = select * from my_to_do_app_todo;
+
+    # template 으로 전달할 dict 구성
+    content = {'todos':todos} # index.html에서 동적으로 코드를 생성할때 dict를 사용
+
+    return render(request,'my_to_do_app/index.html',content) # Templates/my_to_do_app/index.html
     # return HttpResponse('my_to_do_app first page') # 응답하는지 확인할 수 있는 코드
     # 요청에 대한 응답객체를 생성해서 바로 클라이언트로 반환
 
@@ -15,6 +22,16 @@ def createTodo(request) :
     new_todo = Todo(content=user_input_str) # modela에 대입: insert into _todo (content) values (input_str)
     new_todo.save() # DB에 반영
 
-    return HttpResponse('입력한 메모 data는: '+ user_input_str + "는 DB에 저장되었습니다.")
+    return HttpResponseRedirect(reverse('index')) # 127.0.0.1:8000 요청해야 함
+    # return HttpResponse('입력한 메모 data는: '+ user_input_str + "는 DB에 저장되었습니다.")
     # return HttpResponse('createTodo 메모 작성 합니다~') # 요청에 응답하는지만 확인
+
+def doneTodo(request) :
+    # 삭제할 메모의 id 저장
+    done_todo_id = request.GET['todoNum']
+    print('완료한 메모의 id', done_todo_id)
+    todo = Todo.objects.get(id=done_todo_id) # 넘어온 done_todo_id 의 동일한 기본키를 찾아 해당 레코드를 반환
+    todo.delete() # 반환된 객체에 대해 delete 진행
+    return HttpResponseRedirect(reverse('index'))
+
 
